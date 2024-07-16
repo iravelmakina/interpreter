48af2eea7793c422dceea5112cd2469247642561
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2024-07-16 19:36:53 +0300

    operator precedence and infixToPostfix func
    -added operator precedence
    -created func to transform infix expression to postfix using queue and stack including validation

--- interpreter.cpp
+++ interpreter.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <unordered_map>
+#include <queue>
+#include <stack>
 
 enum TokenType {OPERAND, OPERATOR, LEFT_BRACKET, RIGHT_BRACKET};
 
@@ -52,4 +54,51 @@ std::vector<Token> tokenize(const std::string& expression) {
     }
 
     return tokens;
+}
+
+std::unordered_map<char, int> Precedence = { {'+', 10}, {'-', 10}, {'*', 20}, {'/', 20} };
+
+std::queue<Token> infixToPostfix(const std::vector<Token>& tokens) {
+    std::queue<Token> outputQueue;
+    std::stack<Token> operatorStack;
+
+    for (const Token& token : tokens) {
+        switch(token.type) {
+            case OPERAND:
+                outputQueue.push(token);
+                break;
+
+            case OPERATOR:
+                while (!operatorStack.empty() && operatorStack.top().type != LEFT_BRACKET &&
+                       Precedence[operatorStack.top().op] >= Precedence[token.op]) {
+                    outputQueue.push(operatorStack.top());
+                    operatorStack.pop();
+                }
+                operatorStack.push(token);
+                break;
+
+            case LEFT_BRACKET:
+                operatorStack.push(token);
+                break;
+
+            case RIGHT_BRACKET:
+                while (!operatorStack.empty() && operatorStack.top().type != LEFT_BRACKET) {
+                    outputQueue.push(operatorStack.top());
+                    operatorStack.pop();
+                }
+                if (operatorStack.empty() || operatorStack.top().type != LEFT_BRACKET) {
+                    throw std::runtime_error("Mismatched parentheses");
+                }
+                operatorStack.pop();
+                break;
+        }
+    }
+    while (!operatorStack.empty()) {
+        if (operatorStack.top().type == LEFT_BRACKET) {
+            throw std::runtime_error("Mismatched parentheses");
+        }
+        outputQueue.push(operatorStack.top());
+        operatorStack.pop();
+    }
+    return outputQueue;
 }
